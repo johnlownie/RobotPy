7fd957925a4123fdb3fd21ef590b1e686223ddec
John Lownie <jlownie@rogers.com>
2019-02-04 08:08:27 -0500

    added autonomous sendable choosers

--- src/robot.py
+++ src/robot.py
@@ -5,6 +5,7 @@ import wpilib
 
 from networktables import NetworkTables
 from networktables import NetworkTablesInstance
+from wpilib import SendableChooser
 from wpilib import Timer
 from wpilib.command import Scheduler
 from wpilib.shuffleboard import Shuffleboard
@@ -25,7 +26,31 @@ class MyRobot(wpilib.TimedRobot):
         self.slider = Slider(self)
 
         self.oi = OI(self)
- 
+
+        # setup autonomous type
+        scAutonomousType = wpilib.SendableChooser()
+        scAutonomousType.setDefaultOption("1. Driver Controlled", 100)
+        scAutonomousType.addOption       ("2. Timed Just Drive Forward", 200)
+        scAutonomousType.addOption       ("3. Motion Profile Just Drive Forward", 300)
+        scAutonomousType.addOption       ("4. Total Autonomous", 400)
+        wpilib.SmartDashboard.putData    ("Anonomous Type", scAutonomousType)   
+
+        # setup robot positions
+        scRobotPosition = wpilib.SendableChooser()
+        scRobotPosition.setDefaultOption("1. Upper Left" , 1)
+        scRobotPosition.addOption       ("2. Lower Left" , 2)
+        scRobotPosition.addOption       ("3. Centre"     , 3)
+        scRobotPosition.addOption       ("4. Upper Right", 4)
+        scRobotPosition.addOption       ("5. Lower Right", 5)
+        wpilib.SmartDashboard.putData   ("Robot Position", scRobotPosition)
+
+        # setup hatch positions
+        scHatchPosition = wpilib.SendableChooser()
+        scHatchPosition.setDefaultOption("1. Back"       , 10)
+        scHatchPosition.addOption       ("2. Middle"     , 20)
+        scHatchPosition.addOption       ("3. Front"      , 30)
+        wpilib.SmartDashboard.putData   ("Hatch Position", scHatchPosition)
+
         # wpilib.SmartDashboard.putData(self.drivetrain)
 
     def teleopInit(self):
